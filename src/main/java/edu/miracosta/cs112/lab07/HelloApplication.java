e0f9191751092135bd3dc40b7d7cde4649152da2
Gwen Gilbert <laygilbert07@gmail.com>
2025-04-01 12:49:37 -0700

    Added EventHandler

--- src/main/java/edu/miracosta/cs112/lab07/HelloApplication.java
+++ src/main/java/edu/miracosta/cs112/lab07/HelloApplication.java
@@ -1,6 +1,7 @@
 package edu.miracosta.cs112.lab07;//package name here depending on your IDE
 
 import javafx.application.Application;  //abstract class used for JavaFX GUI's
+import javafx.event.Event;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;              //class for GUI window
@@ -11,9 +12,12 @@ import javafx.scene.control.Button;     //class for button component
 import javafx.event.EventHandler;       //interface for handling events
 import javafx.event.ActionEvent;        //class for type of event for action (like button or key pressed)
 
-public class HelloApplication extends Application  { //inheriting core functionality + this class will handle events
+public class HelloApplication extends Application implements EventHandler { //inheriting core functionality + this class will handle events
     /*** GUI COMPONENTS ***/
-    // TODO: follow step 25 in README.md to create reference variables
+    public Label label1;
+    public Label label2;
+    public Button button1;
+    public Button button2;
 
     /*** DRIVER main ***/
     public static void main(String[] args) {
@@ -30,39 +34,46 @@ public class HelloApplication extends Application  { //inheriting core functiona
 
         AnchorPane anchor = new AnchorPane();
 
-        Label label = new Label();
-        label.setText("Hello GUI World");
-        Label label2 = new Label();
+        label1 = new Label();
+        label1.setText("Hello GUI World");
+        label2 = new Label();
         label2.setText("Hello GUI World 2");
 
-        Button button = new Button();
-        button.setText("Don't.");
+        button1 = new Button();
+        button1.setText("Don't.");
 
-        Button button2 = new Button();
+        button2 = new Button();
         button2.setText("Click Me!");
 
-        AnchorPane.setTopAnchor(label, 10.0);
-        AnchorPane.setRightAnchor(label, 10.0);
+        AnchorPane.setTopAnchor(label1, 10.0);
+        AnchorPane.setRightAnchor(label1, 10.0);
 
         AnchorPane.setTopAnchor(label2, 10.0);
         AnchorPane.setLeftAnchor(label2, 10.0);
 
-        AnchorPane.setBottomAnchor(button, 10.0);
-        AnchorPane.setRightAnchor(button, 10.0);
+        AnchorPane.setBottomAnchor(button1, 10.0);
+        AnchorPane.setRightAnchor(button1, 10.0);
 
         AnchorPane.setBottomAnchor(button2, 10.0);
         AnchorPane.setLeftAnchor(button2, 10.0);
 
-        anchor.getChildren().add(label);
+        anchor.getChildren().add(label1);
         anchor.getChildren().add(label2);
-        anchor.getChildren().add(button);
+        anchor.getChildren().add(button2);
         anchor.getChildren().add(button2);
 
         stack.getChildren().add(anchor);
         stage.show();
     }
 
-    // TODO: follow steps 10-21 in README.md to add objects to your layout (inside start)
+    @Override
+    public void handle(Event event) {
+        if (event.getSource() == button1) {
+            label1.setText("New Text!");
+        }
+        else if (event.getSource() == button2) {
+            label2.setText("New Text 2!");
+        }
+    }
 
-    // TODO: follow steps 22-34 in README.md to create an event handler
 }
